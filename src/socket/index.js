27fdc9ef6c9322a4ed3cb68a176b6232448525d9
向文城 <xiangwencheng@fadada.com>
2023-11-06 15:44:25 +0800

    房间

--- src/socket/index.js
+++ src/socket/index.js
@@ -9,6 +9,35 @@ function initSocket(httpServer){
 
   socketServer.on('connection', (socket) => {
     // console.log('socket', socket);
+    console.log('connection');
+    let userInfo = null
+    let myRoom = null
+    socket.on('join',(obj,callback)=>{
+      userInfo = obj
+      socket.join(userInfo.roomId)
+      myRoom = socketServer.sockets.adapter.rooms.get(userInfo.roomId)
+      const roomSize = myRoom.size
+      callback(roomSize)
+      socket.to(userInfo.roomId).emit('changeRoomSize',roomSize)
+      // setTimeout(()=>{
+      //   console.log('欢迎你');
+      //   socketServer.emit('news',"欢迎你！")
+      // },3000)
+      // setTimeout(()=>{
+      //   socket.disconnect()
+      // },3000)
+    })
+
+    socket.on('sendMsg',(msg,callback)=>{
+      const time = +new Date() + ''
+      const msgObj = {...userInfo,msg,time}
+      callback(msgObj)
+      socket.to(userInfo.roomId).emit('sendMsg',msgObj)
+    })
+
+    socket.on('disconnect',()=>{
+      socket.to(userInfo.roomId).emit('changeRoomSize',myRoom.size)
+    })
   })
 }
 
