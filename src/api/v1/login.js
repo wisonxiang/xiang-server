9f0a6891ad7ddefb4378e1dd4fe0bac10374e8ac
向文城 <xiangwencheng@fadada.com>
2023-11-09 11:33:57 +0800

    登录注册逻辑

--- src/api/v1/login.js
+++ src/api/v1/login.js
@@ -1,28 +1,17 @@
 import router from "./init.js";
-import { decrypt, encrypt } from "@utils/crypto.js";
 import sql from '@utils/db.js'
 import { validateLogin, validateRegster } from '@utils/validations.js'
 
 router.post('/login', (ctx) => {
   const params = ctx.request.body
-  validateLogin(params)
-  const t = params.passwd.slice(-13)
-  const passwd = params.passwd.split(t)[0]
-  const privateKey = decrypt(passwd, t)
-  const realPasswd = encrypt(privateKey)
+  const { username, realPasswd } = validateLogin(params)
   console.log('realPasswd', realPasswd);
   ctx.success('登录成功')
 })
 
 router.post('/register', async (ctx) => {
   const params = ctx.request.body
-  validateRegster(params)
-  const t = params.passwd.slice(-13)
-  const passwd = params.passwd.split(t)[0]
-  const privateKey = decrypt(passwd, t)
-  const realPasswd = encrypt(privateKey)
-  const username = params.username
-  const email = params.email || null
+  const { username, realPasswd, email } = validateRegster(params)
   try {
     const user = await sql.query('select * from user where username = ?;', [username])
     if (user[0].length) {
--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,29 +1,35 @@
 import validator from 'validator';
 import { HttpException } from '@utils/exceptions.js'
+import { decrypt, encrypt } from "@utils/crypto.js";
 
-function responseError(msg,code){
-  throw new HttpException(msg,code)
+function responseError(msg, code) {
+  throw new HttpException(msg, code)
 }
 
-export function validateLogin(params){
+export function validateLogin(params) {
   const { username, passwd } = params
   const t = passwd.slice(-13)
   const cT = +new Date()
-
-  if(!username){
+  const pd = passwd.split(t)[0]
+  const privateKey = decrypt(pd, t)
+  if (!username) {
     return responseError('用户名不能为空')
   }
-  if(!passwd){
+  if (!privateKey) {
     return responseError('密码不能为空')
   }
   if (cT - +t > 60000) {
-   return responseError('密码过期')
+    return responseError('密码过期')
   }
+  const realPasswd = encrypt(privateKey)
+  return { username, realPasswd }
 }
 
 export function validateRegster(params) {
-  validateLogin(params)
+  const { username, realPasswd } = validateLogin(params)
+  const { email } = params
   if (validator.isEmail(email)) {
     return responseError('邮箱不合法')
   }
+  return { username, realPasswd, email }
 }
