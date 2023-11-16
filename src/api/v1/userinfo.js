b9f7c8ae9825e8fe177cf56713a6ffe53acf7491
向文城 <xiangwencheng@fadada.com>
2023-11-16 13:53:24 +0800

    登录鉴权

--- src/api/v1/userinfo.js
+++ src/api/v1/userinfo.js
@@ -1,7 +1,10 @@
 import router from "./init.js";
+import sql from '@utils/db.js'
 
-router.get('/userinfo',(ctx)=>{
-  ctx.success('success', 0, ctx.userinfo)
+router.get('/userinfo',async (ctx)=>{
+  const res = await sql.query('select uid,username,email from user where uid = ?', [ctx.uid]);
+  const userinfo  = res[0][0]
+  ctx.success('success', 0, userinfo)
 })
 
 export default router
--- src/middlewares/localFilter.js
+++ src/middlewares/localFilter.js
@@ -1,6 +1,5 @@
 import { HttpException } from '@utils/exceptions.js'
 import { jwtVerify } from '@utils/jwt'
-import sql from '@utils/db.js'
 //定义允许直接访问的url
 const allowPages = ['/api/v1/login','/api/v1/register']
 //拦截
@@ -14,13 +13,12 @@ export default async function localFilter(ctx,next) {
           throw new HttpException('token不能为空',403)
         }
         try {
-          const tokenObj =  jwtVerify(token)
-          const res = await sql.query('select uid,username,email from user where uid = ?', [tokenObj.uid]);
-          ctx.userinfo = res[0][0]
+          const jwtObj = jwtVerify(token)
+          ctx.uid = jwtObj.uid
+          await next()
         } catch (error) {
           console.log('err',error);
           throw new HttpException('token校验失败',403)
         }
-        await next()
     }
 }
